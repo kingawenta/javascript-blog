36a72f4da92e76a0ab571c1ac586dadc11912f10
Kinga Wenta <kinga.wenta@gmail.com>
2021-03-27 23:00:25 +0100

    modification

--- js/script.js
+++ js/script.js
@@ -37,3 +37,35 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML ='';
+  /* for each article */
+const articles = document.querySelectorAll(optArticleSelector);
+let html = '';
+for(let article of articles){
+    /* get the article id */
+const articleId = article.getAttribute('id');
+    /* find the title element */
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+    const linkHTMLData = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+    linkHTML.insertAdjacentHTML('afterend', linkHTML);
+    /* insert link into titleList */
+    html = html + linkHTML;
+  }
+  const links = document.querySelectorAll('.titles a');
+}
+
+
+generateTitleLinks();
