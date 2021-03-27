8879aaacd7832633ce291534f9b3e2acd05a2c20
Kinga Wenta <kinga.wenta@gmail.com>
2021-03-27 19:40:50 +0100

    active clickedElement

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
 
   /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -11,7 +11,8 @@ function titleClickHandler(event){
     activeLink.classList.remove('active');
   }
   /* add class 'active' to the clicked link */
-
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
   /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('article.active');
 
